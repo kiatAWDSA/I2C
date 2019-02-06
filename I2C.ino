784b0255395e5a7c402bba127f083349f9742ac6
kiatAWDSA <lskiat@huskers.unl.edu>
2019-02-06 14:28:47 -0600

    Add project files.

---
+++ I2C.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		I2C.ino
+ Created:	3/27/2018 9:07:27 AM
+ Author:	slau4
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "I2C.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
